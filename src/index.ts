0a12c6a3c1fec524fc416a44b5765bbfb89d17af
Dartagnan <solucoesconsultoria542@gmail.com>
2025-07-24 14:59:47 -0300

    check db connection and data transfer

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
+import { prisma } from "../database/prisma";
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
+  .get("/products", async () => {
+    const products = await prisma.product.findMany();
+    return products;
+  })
   .listen(process.env.PORT || 5000);
 
 console.log(
